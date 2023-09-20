882e7241a744651ab77f51ff80bc9f6585a70796
Shohanojjaman <shohanojjamanemon@gmail.com>
2023-09-21 01:08:42 +0600

    Added Loading spinner.

--- src/components/Layouts/Layout.jsx
+++ src/components/Layouts/Layout.jsx
@@ -1,14 +1,23 @@
-import { Outlet } from 'react-router-dom';
+import { Outlet, useNavigation } from 'react-router-dom';
 import Footer from '../Footer/Footer';
 import Header from '../Header/Header';
 
 const Layout = () => {
+  const navigation = useNavigation();
+  console.log(navigation.state);
   return (
     <>
       <Header></Header>
-      <main>
-        <Outlet></Outlet>
+      <main className="min-h-screen">
+        {navigation.state === 'loading' ? (
+          <div className="flex justify-center items-center h-screen">
+            <span className="loading loading-infinity loading-lg"></span>
+          </div>
+        ) : (
+          <Outlet></Outlet>
+        )}
       </main>
+
       <Footer></Footer>
     </>
   );
