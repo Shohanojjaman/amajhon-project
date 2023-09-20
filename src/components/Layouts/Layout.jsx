059dcb0d0c376fab10e6145d8e21498b603a027e
Shohanojjaman <shohanojjamanemon@gmail.com>
2023-09-20 21:09:51 +0600

    Created Header.jsx and added to Layout.jsx

--- src/components/Layouts/Layout.jsx
+++ src/components/Layouts/Layout.jsx
@@ -1,10 +1,12 @@
 import { Outlet } from 'react-router-dom';
+import Header from '../Header/Header';
 
 const Layout = () => {
   return (
-    <div>
+    <>
+      <Header></Header>
       <Outlet></Outlet>
-    </div>
+    </>
   );
 };
 
