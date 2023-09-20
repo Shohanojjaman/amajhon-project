00b057e544e9e48fb7bb4f6e682a486db0adf636
Shohanojjaman <shohanojjamanemon@gmail.com>
2023-09-20 20:18:02 +0600

    Modified main routes.

--- src/routes/MainRoutes.jsx
+++ src/routes/MainRoutes.jsx
@@ -1,5 +1,5 @@
 import { createBrowserRouter } from 'react-router-dom';
-import Layout from '../components/Layouts/layout';
+import Layout from '../components/Layouts/Layout';
 
 const MainRoutes = createBrowserRouter([
   {
