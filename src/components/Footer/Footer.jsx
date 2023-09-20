b4ef50339a11309bc2e25396b02a431e5450906d
Shohanojjaman <shohanojjamanemon@gmail.com>
2023-09-20 22:41:14 +0600

    Fixed Footer reponsive issue.

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,14 +2,14 @@ import { Link } from 'react-router-dom';
 
 const Footer = () => {
   return (
-    <footer className="footer items-center p-4 container mx-auto max-sm:px-5">
+    <footer className="footer max-md:justify-center items-center py-8 md:py-6 container mx-auto max-sm:px-5 max-md:gap-y-1">
       <aside className="items-center grid-flow-col">
         <Link to="/" className="btn btn-ghost normal-case text-2xl">
           Amajhon
         </Link>
         <p>Copyright © 2023 - All right reserved</p>
       </aside>
-      <nav className="grid-flow-col gap-4 md:place-self-center md:justify-self-end">
+      <nav className="grid-flow-col gap-4 md:place-self-center justify-self-center md:justify-self-end">
         <ul className="menu menu-horizontal px-1">
           <li>
             <Link className="text-lg" to="/products">
